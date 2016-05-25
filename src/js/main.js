e1888838d82a1f701d86eec50058fe949cdc295e
OUYANG Weiwei <mathieuouyang@gmail.com>
2016-05-25 08:29:11 +0200

    navbar and parallax

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
-jQuery(window).load(function () {
-    jQuery(".preloader").delay(5000).fadeOut("slow");
+$(window).load(function () {
+    $(".preloader-wrapper").delay(500).fadeOut();
+    $(".preloader").delay(1000).fadeOut("slow");
+});
+
+$(document).ready(function () {
+    $(".button-collapse").sideNav();
+    $('.parallax').parallax();
 });
