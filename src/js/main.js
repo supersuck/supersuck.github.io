6eee3571ddcc004eb0b398e4a80786fce0a83bd6
Weiwei OUYANG <i@wouyang.com>
2016-05-25 17:58:04 +0200

    Parallax intergration && float button

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 $(window).load(function () {
-    $(".preloader-wrapper").delay(500).fadeOut();
-    $(".preloader").delay(1000).fadeOut("slow");
+    $('.preloader-wrapper').delay(500).fadeOut();
+    $('.preloader').delay(1000).fadeOut("slow");
 });
 
 $(document).ready(function () {
-    $(".button-collapse").sideNav();
+    $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    
+    $('#toggle_color').click(function () {
+       $('.blue').toggleClass('teal', 'blue');
+    });
 });
