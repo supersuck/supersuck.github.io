76cc14aa328af51c17fdfaee9ec7dc2f3845d321
Weiwei OUYANG <i@wouyang.com>
2016-05-26 21:43:41 +0200

    color for contact form

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,25 @@ $(window).load(function () {
 $(document).ready(function () {
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    
+
     $('#toggle_color').click(function () {
-       $('.blue').toggleClass('teal', 'blue');
-       $('.blue-text').toggleClass('teal-text', 'blue-text');
+        $('.blue').toggleClass('teal', 'blue');
+        $('.blue-text').toggleClass('teal-text', 'blue-text');
     });
 });
 
+// Smooth Scroll
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
